150c43031155eaddf7fefca8ea887b71fb78a1f6
WSCWNeel <159732843+WSCWNeel@users.noreply.github.com>
2024-05-02 10:28:00 +1200

    lol

---
+++ main.py
@@ -0,0 +1,12 @@
+print ("Hello")
+#Question of User's Name and the code remembering the answer
+name= input("What is your name?")
+#Greeting the User and introducing the quiz
+print("Welcome to the Quiz", name)
+print("This quiz is about something")
+#Asking the user a question
+input("What is your favourite color?")
+#Telling the user the correct answer
+print("The correct answer was Blue")
+#Ending the Quiz
+print("Thank you for doing this quiz and goodbye")
