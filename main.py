7c4bea3fb681fe2aee0277f46db5399af869c0ad
WSCWNeel <159732843+WSCWNeel@users.noreply.github.com>
2024-05-07 09:03:56 +1200

    Finished V2 python quiz along with V1

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+score = 0
 print ("Hello")
 #Question of User's Name and the code remembering the answer
 name= input("What is your name?")
 #Greeting the User and introducing the quiz
 print("Welcome to the Quiz", name)
-print("This quiz is about something")
+print("This quiz is about guessing your favourite color according to me")
 #Asking the user a question
-input("What is your favourite color?")
+answer=input("What is your favourite color?")
 #Telling the user the correct answer
-print("The correct answer was Blue")
+if answer == " Blue":
+    print("Correct!") 
+elif answer == "":
+    print("Not Sure?")
+    print("It's Blue")
+else:
+    print("Wrong answer")
+    print("The correct answer was the color Blue")
+
+if answer == " Blue":
+    score =+ 100
 #Ending the Quiz
+print("Your final score is", score)
 print("Thank you for doing this quiz and goodbye")
